7ef43a79ab75aca65ab3aef6019c98daa6839dd6
Natalia-githu <78275767+Natalia-githu@users.noreply.github.com>
2021-04-14 22:22:46 +0300

    less2

---
+++ students/Zemlyanova/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        app: './index.jsx',
+    },
+    context: path.resolve(__dirname, "static_src"),
+    output: {
+        path: path.resolve(__dirname, "static", "build"),
+        filename: 'app.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                include: path.resolve(__dirname, "static_src"),
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/env','@babel/react'],
+                    plugins: [
+                        [
+                            "@babel/plugin-proposal-class-properties",
+                            {
+                                "loose": true
+                            }
+                        ]
+                    ]
+
+                }
+
+            },
+        ],
+    },
+};
