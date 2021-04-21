abf1f28d9999cc55871c5cf704758915a83a4363
Natalia-githu <78275767+Natalia-githu@users.noreply.github.com>
2021-04-21 22:08:58 +0300

    less#4

--- students/Zemlyanova/src/components/ChatList.jsx
+++ students/Zemlyanova/src/components/ChatList.jsx
@@ -1,20 +1,65 @@
 import React from 'react';
 import { List, ListItem } from 'material-ui/List';
 import ContentSend from 'material-ui/svg-icons/content/send';
-
+import { Link } from 'react-router-dom';
+import PropTypes from "prop-types";
+import AddIcon from 'material-ui/svg-icons/content/add';
+import { TextField } from 'material-ui';
 
 export default class ChatList extends React.Component {
+    static propTypes = {
+        chats: PropTypes.object.isRequired,
+        addChat: PropTypes.func.isRequired,
+    };
+    state = {
+        input: '',
+    };
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    handleKeyUp = (event) => {
+        if (event.keyCode === 13) { // Enter
+            this.handleAddChat();
+        }
+    };
+
+    handleAddChat = () => {
+        if (this.state.input.length > 0) {
+            this.props.addChat(this.state.input);
+            this.setState({ input: '' });
+        }
+    };
+
     render() {
+        const { chats } = this.props;
+        const chatElements = Object.keys(chats).map(chatId => (
+            <Link key={ chatId } to={ `/chat/${chatId}` }>
+                <ListItem
+                    primaryText={ chats[chatId].title }
+                    leftIcon={ <ContentSend /> } />
+            </Link>));
+
         return (
             <List>
 
-                    <ListItem primaryText="Chat 1" leftIcon={<ContentSend />} />
+                { chatElements }
+                <ListItem
+                    key="Add new chat"
+                    leftIcon={ <AddIcon /> }
+                    onClick={ this.handleAddChat }
+                    style={ { height: '60px' } }
+                    children= {<TextField
+                        key="textField"
+                        fullWidth
+                        name="input"
+                        hintText="Добавить новый чат"
+                        onChange={ this.handleChange }
+                        value={ this.state.input }
+                        onKeyUp={ this.handleKeyUp }
+                    />}
+                />
 
-
-                    <ListItem primaryText="Chat 2" leftIcon={<ContentSend />} />
-
-
-                    <ListItem primaryText="Chat 3" leftIcon={<ContentSend />} />
 
             </List>
         )
